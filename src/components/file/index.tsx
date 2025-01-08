4edf6d71dafffbc9a72f90401debed515c7e53f8
trinhpt <phamtientrinh4@gmail.com>
2025-01-08 09:18:22 +0700

    2

--- src/components/file/index.tsx
+++ src/components/file/index.tsx
@@ -1,15 +1,15 @@
-const InputFile = ({ isOpen }) => {
-  return (
-    <div>
-      <input
-        type="file"
-        id="fileInput"
-        accept="image/*,application/pdf"
-        multiple
-        onChange={(e) => handleFileChange(e)}
-      />
-      <button onClick={handleSendFile}>Gửi</button>
-    </div>
-  );
-};
-export default InputFile;
+// const InputFile = ({ isOpen }) => {
+//   return (
+//     <div>
+//       <input
+//         type="file"
+//         id="fileInput"
+//         accept="image/*,application/pdf"
+//         multiple
+//         onChange={(e) => handleFileChange(e)}
+//       />
+//       <button onClick={handleSendFile}>Gửi</button>
+//     </div>
+//   );
+// };
+// export default InputFile;
