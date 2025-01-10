658c38e66b58636bf0bbf68fd65f98fd55c22b77
trinhpt <phamtientrinh4@gmail.com>
2025-01-10 09:15:46 +0700

    2

--- src/validate/filterData.tsx
+++ src/validate/filterData.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 type DataType = {
   id: string;
   data: {
-    users: string[];
+    users?: string[];
   };
 };
 function FilterData(value: { users: DataType }) {
@@ -13,7 +13,7 @@ function FilterData(value: { users: DataType }) {
   useEffect(() => {
     if (loggedInUser) {
       // Tìm kiếm user phù hợp
-      const foundUser = value.users.data.users
+      const foundUser = value?.users?.data?.users
         .map((item) => item)
         .flat()
         .find((email) => email !== loggedInUser?.email);
